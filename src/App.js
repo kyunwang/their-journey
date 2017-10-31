e5d52f75bd2f381747e85ded4070345d096098fd
Kangyun Wang <wky@192.168.2.44>
2017-10-31 18:24:42 +0100

    Added package. Small setup

--- src/App.js
+++ src/App.js
@@ -3,19 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+	constructor() {
+		super();
+		this.state = {
+			screenWidth: 1000,
+			screenHeight: 1000,
+		}
+	}
+
+	onResize = () => {
+		this.setState({
+			screenWidth: window.innerWidth,
+			screenHeight: window.innerHeight - 120
+		});
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1 className="App-title">Welcome to React</h1>
+				</header>
+				<p className="App-intro">
+					To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
-    );
-  }
+			</div>
+		);
+	}
 }
 
 export default App;
