ef3afeacac5aab07dcede62c7962f7de659429da
Kangyun Wang <wky@kangyuns-mbp.home>
2017-11-01 19:34:38 +0100

    Deleted unused files

--- src/App.js
+++ src/App.js
@@ -18,12 +18,17 @@ class App extends Component {
 		});
 	}
 
+	componentDidMount() {
+		window.addEventListener('resize', this.onResize, false)
+		this.onResize()
+	}
+
 	render() {
 		return (
 			<div className="App">
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to React</h1>
+					<h1 className="App-title">Welcome to React Refugee</h1>
 				</header>
 				<p className="App-intro">
 					To get started, edit <code>src/App.js</code> and save to reload.
