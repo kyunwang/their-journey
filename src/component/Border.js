3ed2280946b0eee126f3d340e9cccf4374d8b6e2
Kangyun Wang <wky@kangyuns-mbp.home>
2017-11-03 14:06:12 +0100

    Cleaned test data. Projection in a own file

---
+++ src/component/Border.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { geoPath } from 'd3-geo';
+
+class Border extends Component {
+	render() {
+		const {
+			svgHeight,
+			svgWidth,
+			projection,
+			worldData,
+		} = this.props;
+
+		console.log(this.props);
+		return (
+			<svg
+				height={svgHeight}
+				width={svgWidth}
+				viewBox={`0 0 ${svgWidth - 200} ${svgHeight + 400}`}
+			>
+
+				<g className="countries">
+					{
+						worldData.map((d, i) => {
+							{ /* console.log(d); */ }
+							return (<path
+								// onMouseEnter={() => { console.log('enterrerd'); }}
+								key={d.properties.BRK_NAME}
+								d={geoPath().projection(this.props.projection())(d)}
+								className="country"
+								fill={'#a3a3a3'}
+								// fill={`rgba(38,50,56,${1 / worldData.length * i})`}
+								stroke="#FFFFFF"
+								strokeWidth={0.5}
+							/>);
+						})
+					}
+				</g>
+
+				{/*
+			<g className="markers">
+				<circle
+					cx={this.projection()([8, 48])[0]}
+					cy={this.projection()([8, 48])[1]}
+					r={10}
+					fill="#E91E63"
+					className="marker"
+				/>
+			</g> */}
+			</svg>
+		);
+	}
+}
+
+export default Border;
--- src/component/WorldMap.js
+++ src/component/WorldMap.js
@@ -1,9 +1,22 @@
-// From https://medium.com/@zimrick/how-to-create-pure-react-svg-maps-with-topojson-and-d3-geo-e4a6b6848a98
+// Base worldmap from  https://medium.com/@zimrick/how-to-create-pure-react-svg-maps-with-topojson-and-d3-geo-e4a6b6848a98
 
 import React, { Component } from 'react';
-import { geoMercator, geoPath } from 'd3-geo';
+import {
+	geoMercator,
+	geoPath,
+} from 'd3-geo';
+import {
+	json as d3Json,
+	csv as d3Csv,
+	text as d3Text,
+} from 'd3-request';
+
 import { feature } from 'topojson-client';
 import PropTypes from 'prop-types';
+
+import Border from './Border';
+
+import { cleanRefugee } from '../cleanData';
 
 class WorldMap extends Component {
 	static propTypes = {
@@ -19,38 +32,35 @@ class WorldMap extends Component {
 	constructor() {
 		super();
 		this.state = {
-			worldData: [],
+			worldData: null,
 		};
 	}
 
 	componentDidMount() {
-		// fetch("/europe.json") // objects.europe
-		// fetch("/world-110m.json")
-		fetch('/ne_10m_admin_0_countries.json')
-			.then(response => {
-				if (response.status !== 200) {
-					console.log(`There was a problem: ${response.status}`);
-					return;
-				}
-
-				response.json().then(worldData => {
-					console.log(worldData);
-					this.setState({
-						worldData: feature(worldData, worldData.objects.countries).features,
-					});
-				});
-			});
+		d3Json('/ne_50m_admin_0_countries_lakes.json', this.loadMap);
+		d3Text('/to_germany_2014.csv', this.loadRefugee);
 	}
 
+	loadMap = (err, res) => {
+		this.setState({
+			worldData: feature(res, res.objects.countries).features,
+		});
+	}
 
-	projection() {
+	loadRefugee = (err, res) => {
+		this.setState({
+			refugeeData: cleanRefugee(res),
+		});
+	}
+
+	projection = () => {
 		const lo = 26.2206322; // x
 		const la = 46.0485818; // y
+
 		return geoMercator()
 			.center([0, la])
 			.rotate([-lo, 0])
 			.scale(this.props.svgWidth * 0.55)
-			// .scale(this.props.svgHeight / 2)
 			.translate([this.props.svgWidth / 2, this.props.svgHeight / 2]);
 	}
 
@@ -60,44 +70,26 @@ class WorldMap extends Component {
 			svgWidth,
 		} = this.props;
 
-		// console.log('props', this.props);
+		const {
+			worldData,
+		} = this.state;
 
+
+		// Can set a loader here
+		if (!worldData) {
+			return (
+				<p>Loading</p>
+			);
+		}
 
 		return (
-			// <svg width={svgWidth} height={svgHeight} viewBox={`0 0 800 450`}>
-			// <svg width={1000} height={800}>
-			<svg
-				height={svgHeight}
-				width={svgWidth}
-				viewBox={`0 0 ${svgWidth} ${svgHeight}`}
-			>
-
-				<g className="countries">
-					{
-						this.state.worldData.map((d, i) => (
-							<path
-								key={`path-${i}`}
-								d={geoPath().projection(this.projection())(d)}
-								className="country"
-								fill={`rgba(38,50,56,${1 / this.state.worldData.length * i})`}
-								stroke="#FFFFFF"
-								strokeWidth={0.5}
-							/>
-						))
-					}
-				</g>
-
-				{/*
-				<g className="markers">
-					<circle
-						cx={this.projection()([8, 48])[0]}
-						cy={this.projection()([8, 48])[1]}
-						r={10}
-						fill="#E91E63"
-						className="marker"
-					/>
-				</g> */}
-			</svg>
+			<div>
+				<Border
+					{...this.props}
+					projection={this.projection}
+					worldData={worldData}
+				/>
+			</div>
 		);
 	}
 }
