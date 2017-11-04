3c3f9363fca3215cfd72ea187f85471d2f400b03
Kangyun Wang <wky@192.168.2.44>
2017-11-04 12:24:13 +0100

    svg container insted of in item

--- src/component/Border.js
+++ src/component/Border.js
@@ -13,41 +13,41 @@ class Border extends Component {
 
 		console.log(this.props);
 		return (
-			<svg
-				height={svgHeight}
-				width={svgWidth}
-				viewBox={`0 0 ${svgWidth - 200} ${svgHeight + 400}`}
-			>
+			// <svg
+			// 	height={svgHeight}
+			// 	width={svgWidth}
+			// 	viewBox={`0 0 ${svgWidth - 200} ${svgHeight + 400}`}
+			// >
 
-				<g className="countries">
-					{
-						worldData.map((d, i) => {
-							{ /* console.log(d); */ }
-							return (<path
-								// onMouseEnter={() => { console.log('enterrerd'); }}
-								key={d.properties.ADMIN}
-								d={geoPath().projection(this.props.projection())(d)}
-								className="country"
-								fill={'#a3a3a3'}
-								// fill={`rgba(38,50,56,${1 / worldData.length * i})`}
-								stroke="#FFFFFF"
-								strokeWidth={0.5}
-							/>);
-						})
-					}
-				</g>
+			<g className="countries">
+				{
+					worldData.map((d, i) => {
+						{ /* console.log(d); */ }
+						return (<path
+							// onMouseEnter={() => { console.log('enterrerd'); }}
+							key={d.properties.ADMIN}
+							d={geoPath().projection(this.props.projection())(d)}
+							className="country"
+							fill={'#a3a3a3'}
+							// fill={`rgba(38,50,56,${1 / worldData.length * i})`}
+							stroke="#FFFFFF"
+							strokeWidth={0.5}
+						/>);
+					})
+				}
+			</g>
 
-				{/*
-			<g className="markers">
-				<circle
-					cx={this.projection()([8, 48])[0]}
-					cy={this.projection()([8, 48])[1]}
-					r={10}
-					fill="#E91E63"
-					className="marker"
-				/>
-			</g> */}
-			</svg>
+			// 	{/*
+			// <g className="markers">
+			// 	<circle
+			// 		cx={this.projection()([8, 48])[0]}
+			// 		cy={this.projection()([8, 48])[1]}
+			// 		r={10}
+			// 		fill="#E91E63"
+			// 		className="marker"
+			// 	/>
+			// </g> */}
+			// </svg>
 		);
 	}
 }
